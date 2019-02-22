f79ec23279b343073155317ebbc90d731d6b59f7
sumagaruda <35711129+sumagaruda@users.noreply.github.com>
2019-02-21 23:45:29 -0600

    Add files via upload

---
+++ Buffer Overflow Analysis/file_fuzzer.py
@@ -0,0 +1,13 @@
+import sys
+
+if len(sys.argv) <= 2:
+	print "[+] python file_fuzzer.py buffer_size filename"
+	sys.exit()
+	
+buffer_size = int(sys.argv[1])
+filename = sys.argv[2]
+buffer_data = "A" * buffer_size
+fi = open(filename, "w")
+fi.write(buffer_data)
+fi.close()
+print "[+] Success!, file {0} generated with {1} bytes".format(filename, buffer_size)
